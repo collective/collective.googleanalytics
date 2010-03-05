901f0cd1dc715eb92f05ccf27356dc89b995f1a1
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-05 19:45:15 +0000

    Modify javascript registration and configuration javascript to be compatible with Plone 3.1.

--- collective/googleanalytics/browser/analytics_config.js
+++ collective/googleanalytics/browser/analytics_config.js
@@ -2,8 +2,8 @@ jq(function () {
     getAnalyticsContainerWidth = function(container) {
         // If we're displaying the report in an expanding dl, we need to expand it
         // before we can get the width.
-        if (jq(container).closest('dl.collapsedBlockCollapsible').length > 0) {
-            var dl_parent = jq(container).closest('dl.collapsedBlockCollapsible');
+        if (jq(container).parents('dl.collapsedBlockCollapsible').length > 0) {
+            var dl_parent = jq(container).parents('dl.collapsedBlockCollapsible').eq(0);
             dl_parent.removeClass('collapsedBlockCollapsible').addClass('expandedBlockCollapsible');
             var container_width = jq(container).innerWidth();
             dl_parent.removeClass('expandedBlockCollapsible').addClass('collapsedBlockCollapsible');
