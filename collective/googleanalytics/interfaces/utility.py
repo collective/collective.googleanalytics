28ec9ae129bacdfdb9100eebda91dfdd484e2d0a
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-05-26 20:25:20 +0000

    Added a fallback for the password field for older versions of zope.schema.

--- collective/googleanalytics/interfaces/utility.py
+++ collective/googleanalytics/interfaces/utility.py
@@ -4,6 +4,39 @@ from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('analytics')
 
+# Older versions of zope.schema don't allow for an unchanged password.
+if not hasattr(schema.Password, 'UNCHANGED_PASSWORD'):
+    class Password(schema.TextLine):
+        """A text field containing a text used as a password."""
+
+        UNCHANGED_PASSWORD = object()
+
+        def set(self, context, value):
+            """Update the password.
+
+            We use a special marker value that a widget can use
+            to tell us that the password didn't change. This is
+            needed to support edit forms that don't display the
+            existing password and want to work together with
+            encryption.
+
+            """
+            if value is self.UNCHANGED_PASSWORD:
+                return
+            super(Password, self).set(context, value)
+
+        def validate(self, value):
+            try:
+                existing = bool(self.get(self.context))
+            except AttributeError:
+                existing = False
+            if value is self.UNCHANGED_PASSWORD and existing:
+                # Allow the UNCHANGED_PASSWORD value, if a password is set already
+                return
+            return super(Password, self).validate(value)
+else:
+    Password = schema.Password
+
 class IAnalyticsCredentials(Interface):
     """
     This interface defines the configlet.
@@ -13,7 +46,7 @@ class IAnalyticsCredentials(Interface):
         description=_(u"Enter the e-mail address used to log in to Google Analytics."),
         required=True)
                                  
-    password = schema.Password(title=_(u"Password"),
+    password = Password(title=_(u"Password"),
         description=_(u"Enter the Google Analytics password for the e-mail address above."),
         required=True)
 
