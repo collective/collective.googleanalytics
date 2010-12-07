dbb0e4603fced39c31dd08f3fe6c25a913f72fd9
yomatters <yomatters@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-12-07 14:26:33 +0000

    Fixed permission bug so that the Google Analytics portlet can be made visible to non-managers.

--- collective/googleanalytics/portlets/analyticsportlet.py
+++ collective/googleanalytics/portlets/analyticsportlet.py
@@ -79,7 +79,7 @@ class Renderer(base.Renderer):
         """
         
         mtool = getToolByName(self.context, 'portal_membership')
-        allowed = mtool.checkPermission('collective.googleanalytics.ViewAnalyticsResults', self.context)
+        allowed = mtool.checkPermission('Google Analytics: View Analytics Results', self.context)
         context_state = self.context.restrictedTraverse('@@plone_context_state')
         return allowed and context_state.is_view_template()
     
