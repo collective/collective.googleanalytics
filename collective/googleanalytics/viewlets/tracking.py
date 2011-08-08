d991e6596aa8992bac4f474d5e84c4b0acd19283
svincic <svincic@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-08-08 19:50:17 +0000

    if the tracking_web_property is set on INavigationRoot object a second tracker is added. More info at http://code.google.com/apis/analytics/docs/tracking/asyncUsageGuide.html#MultipleTrackers

--- collective/googleanalytics/viewlets/tracking.py
+++ collective/googleanalytics/viewlets/tracking.py
@@ -3,6 +3,8 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.analytics.view import AnalyticsViewlet
 from collective.googleanalytics.interfaces.tracking import IAnalyticsTrackingPlugin
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
 
 class AnalyticsTrackingViewlet(AnalyticsViewlet):
     """
@@ -56,4 +58,45 @@ class AnalyticsTrackingViewlet(AnalyticsViewlet):
             if plugin:
                 results.append(plugin())
         return '\n'.join(results)
+
+class AnalyticsSecondaryTrackingViewlet(AnalyticsViewlet):
+    """
+    A viewlet that inserts the Google Analytics tracking code 
+    at the end of the page. We override the default Plone viewlet
+    so that we can exclude the code for certain roles.
+    """
+
+    render = ViewPageTemplateFile('tracking-secondary.pt')
+
+    def __init__(self, context, request, view, manager):
+        super(AnalyticsViewlet, self).__init__(context, request)
+        self.analytics_tool = getToolByName(context, "portal_analytics")
+        self.membership_tool = getToolByName(context, "portal_membership")
+        portal_state = getMultiAdapter((context, request), name=u'plone_portal_state')
+        root = aq_inner(context.restrictedTraverse(portal_state.navigation_root_path()))
+        self.secondary = None
+        if root.hasProperty('tracking_web_property'):
+            self.secondary = root.getProperty('tracking_web_property')
+        
+    def available(self):
+        """
+        Checks to see whether the viewlet should be rendered based on the role
+        of the user and the selections for excluded roles in the configlet.
+        """
+                
+        member = self.membership_tool.getAuthenticatedMember()
+        
+        for role in self.analytics_tool.tracking_excluded_roles:
+            if member.has_role(role) and self.secondary:
+                return False
+        return True
+        
+    def getTrackingWebProperty(self):
+        """
+        Returns the Google web property ID for the selected tracking profile,
+        or an empty string if no tracking profile is selected.
+        """
+
+        return self.secondary
+        
 
