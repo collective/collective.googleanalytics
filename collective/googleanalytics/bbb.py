ae80922602f9b5dc168c71bf43deaaefa5ae73e0
Alex Clark <aclark@aclark.net>
2012-09-27 21:28:13 -0400

    Separatize

--- collective/googleanalytics/bbb.py
+++ collective/googleanalytics/bbb.py
@@ -1,9 +1,13 @@
 # BBB
 try:
     from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
-    from zope.component.hooks import getSite
-    from zope.browser.interfaces import IAdding
 except:
     from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+try:
+    from zope.component.hooks import getSite
+except:
     from zope.app.component.hooks import getSite
+try:
+    from zope.browser.interfaces import IAdding
+except:
     from zope.app.container.interfaces import IAdding
