dfdaef114571c0bb599ed7dce74efee538ac785c
Alex Clark <aclark@aclark.net>
2012-09-27 21:23:43 -0400

    Move IAdding to bbb

--- collective/googleanalytics/bbb.py
+++ collective/googleanalytics/bbb.py
@@ -1,7 +1,9 @@
 # BBB
-try:  # XXX OK to combine these?
+try:
     from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
     from zope.component.hooks import getSite
+    from zope.browser.interfaces import IAdding
 except:
     from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
     from zope.app.component.hooks import getSite
+    from zope.app.container.interfaces import IAdding
--- collective/googleanalytics/browser/interfaces.py
+++
@@ -1,5 +0,0 @@
-# BBB
-try:
-    from zope.browser.interfaces import IAdding
-except:
-    from zope.app.container.interfaces import IAdding
