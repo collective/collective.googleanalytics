9b46fe341da694c839ebbabd21e85337682f2c10
Tom Gross <itconsense@gmail.com>
2013-04-09 01:19:37 +0200

    only whitespaces

--- collective/googleanalytics/tests/test_reports.py
+++ collective/googleanalytics/tests/test_reports.py
@@ -17,20 +17,20 @@ class TestReports(FunctionalTestCase):
         Test that the default expression context used to render TAL and TALES in the
         Analytics report is populated with the correct objects.
         """
-        
+
         analytics_tool = getToolByName(self.portal, 'portal_analytics', None)
         report = analytics_tool['site-visits-line']
 
         context = self.portal
         request = self.portal.REQUEST
-        
+
         renderer = getMultiAdapter(
             (context, request, report),
             interface=IAnalyticsReportRenderer
         )
-                
+
         expression = 'python:[context, request, today, date, timedelta, unique_list]'
-        
+
         result = [
             self.portal,
             self.portal.REQUEST,
@@ -39,43 +39,43 @@ class TestReports(FunctionalTestCase):
             getTimeDelta,
             unique_list,
         ]
-        
+
         exp_context = renderer._getExpressionContext()
         evaluated_exp = evaluateTALES(expression, exp_context)
         self.assertEqual(evaluated_exp, result)
-        
+
     def test_report_returns_correct_results(self):
         """
         Test that the report's getResults method produces an AnalyticsReportResults object
         with the correct values.
         """
-        
+
         analytics_tool = getToolByName(self.portal, 'portal_analytics', None)
         report = analytics_tool['site-visits-line']
-        
+
         context = self.portal
         request = self.portal.REQUEST
-        
+
         # Set the start and end date in the request.
         request.set('start_date', '20100401')
         request.set('end_date', '20100430')
-        
+
         # Load the example feed data from a file.
         feed_xml_file = os.path.join(os.path.dirname(__file__), 'data_feed.xml')
         feed_xml = open(feed_xml_file).read()
         feed = AnalyticsDataFeedFromString(feed_xml)
-            
+
         renderer = getMultiAdapter(
             (context, request, report),
             interface=IAnalyticsReportRenderer
         )
-        
+
         # Set the test data feed.
         renderer._data_feed = feed
-        
+
         # Render the results.
         results = renderer()
-        
+
         # Load the expected results.
         results_js_file = os.path.join(os.path.dirname(__file__), 'report_results.tpl')
         template = Template(open(results_js_file).read())
@@ -83,11 +83,11 @@ class TestReports(FunctionalTestCase):
             'id': renderer._getVisualization().id(),
         }
         results_js = template.substitute(template_vars)
-        
+
         # We normalize all whitespace to spaces to avoid getting false
         # negatives.
         whitespace = re.compile(r'\s+')
-        
+
         # Test that the results match what we expect.
         self.assertEqual(
             re.sub(whitespace, ' ', results).strip(),
