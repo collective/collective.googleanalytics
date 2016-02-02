7aa574046102f21f5cbed5ea1c0089a35a866be5
Ivan Teoh <ivan.teoh@gmail.com>
2016-02-02 14:22:26 +1100

    Add MediaElement.js HTML5 video playback tracking plugin.

--- src/collective/googleanalytics/tracking/plugins.py
+++ src/collective/googleanalytics/tracking/plugins.py
@@ -9,18 +9,18 @@ class AnalyticsBaseTrackingPlugin(object):
     """
     Base plugin for tracking information in Google Analytics.
     """
-    
+
     implements(IAnalyticsTrackingPlugin)
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
-        
+
     def relative_url(self):
         """
         Returns the relative URL of the request.
         """
-        
+
         relative_url = self.request.ACTUAL_URL.replace(self.request.SERVER_URL, '').strip()
         if relative_url.endswith('/') and len(relative_url) > 1:
             return relative_url[:-1]
@@ -30,44 +30,44 @@ class AnalyticsExternalLinkPlugin(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track external links.
     """
-    
+
     __call__ = ViewPageTemplateFile('external.pt')
-    
+
 class AnalyticsEmailLinkPlugin(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track e-mail links.
     """
 
     __call__ = ViewPageTemplateFile('email.pt')
-    
+
 class AnalyticsDownloadPlugin(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track file downloads.
     """
 
     __call__ = ViewPageTemplateFile('download.pt')
-    
+
     file_extensions = json_serialize(FILE_EXTENSION_CHOICES)
-    
+
 class AnalyticsCommentPlugin(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track posting of comments.
     """
 
     __call__ = ViewPageTemplateFile('comment.pt')
-    
+
 class AnalyticsUserTypePlugin(AnalyticsBaseTrackingPlugin):
     """
     A tracking plugin to track user type as a custom variable.
     """
 
     __call__ = ViewPageTemplateFile('usertype.pt')
-    
+
     def user_type(self):
         """
         Returns Member if the user is logged or Visitor otherwise.
         """
-        
+
         membership = getToolByName(self.context, 'portal_membership')
         if membership.isAnonymousUser():
             return 'Visitor'
@@ -79,3 +79,10 @@ class AnalyticsPageLoadTimePlugin(AnalyticsBaseTrackingPlugin):
     """
 
     __call__ = ViewPageTemplateFile('pageloadtime.pt')
+
+class AnalyticsMediaElementJsPlugin(AnalyticsBaseTrackingPlugin):
+    """
+    A tracking plugin to track HTML5 video playback from MediaElement.js.
+    """
+
+    __call__ = ViewPageTemplateFile('mediaelementjs.pt')
