95fa8e941757a2c69d238f0087383595bda93980
Dylan Jay <software@pretaweb.com>
2020-05-27 18:37:27 +0700

    better way to convert to v4 reports api

--- src/collective/googleanalytics/utility.py
+++ src/collective/googleanalytics/utility.py
@@ -35,6 +35,7 @@ from httplib import ResponseNotReady
 from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 import requests
+import gav4
 
 logger = logging.getLogger('collective.googleanalytics')
 
@@ -163,7 +164,10 @@ class Analytics(PloneBaseTool, IFAwareObjectManager, OrderedFolder):
             self._update_credentials(creds)
 
         service = build('analytics', 'v3', credentials=creds)
-        return creds, service
+        reportingservice = build('analyticsreporting', 'v4', credentials=creds)
+        gav4.apply_gav4(reportingservice)
+
+        return creds, service, reportingservice
 
     @security.private
     @ram.cache(account_feed_cachekey)
@@ -196,7 +200,7 @@ class Analytics(PloneBaseTool, IFAwareObjectManager, OrderedFolder):
         #     timeout = DEFAULT_TIMEOUT
         #     logger.warning('Conflict while setting socket timeout.')
 
-        creds, service = self._getService()
+        creds, service, reportingservice = self._getService()
 
         try:
             # socket.setdefaulttimeout(GOOGLE_REQUEST_TIMEOUT)
@@ -209,7 +213,7 @@ class Analytics(PloneBaseTool, IFAwareObjectManager, OrderedFolder):
                 elif api_request == 'profiles':
                     result = service.management().profiles().list(accountId='~all', webPropertyId='~all').execute()
                 elif api_request == 'data':
-                    result = service.data().ga().get(**query_args).execute()
+                    result = reportingservice.gav4_get(**query_args).execute()
                 else:
                     raise ValueError("Not supported api")
                 self._update_credentials(creds)
